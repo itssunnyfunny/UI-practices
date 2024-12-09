f0ada73628b7b8809f1d1a28a14bb280674a2d6f
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-09 14:24:45 +0530

    userReview page is added

---
+++ 3next-auth/components/Home/UserReview.tsx
@@ -0,0 +1,25 @@
+
+
+function UserReview() {
+  return (
+    <div>
+    <div className="mt-20 flex ">
+      <div className="w-[50%] text-center">photo</div>
+      
+      <div className="w-[50%]">
+        <div className="text-sm font-semibold opacity-75">WHAT OUR USERS SAY ABOUT NINOX
+          </div>
+          <div className="mt-8 text-5xl font-medium">Brilliant and easy database. The very easy way to build my oun database without IT-knowledge.</div>
+          <div className="mt-10 text-sm font-semibold">Denny Schuckart</div>
+          <div className="mt-2 text-sm opacity-75 max-w-[250px]">Managing Director Schuckart Kohnert Consultionn GmbH</div>
+          <div className="mt-14"> <div className=" flex gap-6">
+       <div className=" border-2 bg-blue-600 rounded-full p-3" >😶‍🌫️</div>
+       <div className=" border-2 bg-blue-600 rounded-full p-3" >🥶</div>
+       </div></div>
+          </div>
+    </div>
+   </div>
+  )
+}
+
+export default UserReview
