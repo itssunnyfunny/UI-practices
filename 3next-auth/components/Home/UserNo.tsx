23c0778bb38b4aa2d710ae384b277b6d0e90cf2f
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-09 14:26:09 +0530

    number of user page is added

---
+++ 3next-auth/components/Home/UserNo.tsx
@@ -0,0 +1,18 @@
+import Button from "../Button"
+
+function UserNo() {
+  return (
+    <div className="mt-14">
+    <div className=" flex ">
+      <div className="w-[50%]">
+        <div className="text-5xl font-semibold">Over 250,000 people <br /> use Ninox. join our <br /> community!</div>
+        <div className=" mt-14"> <Button  children="start free trial"/></div>
+        </div>
+      <div className=" w-[50%] text-center"> svg </div>
+    </div>
+  </div>
+
+  )
+}
+
+export default UserNo
