5195a6f71252a04ab20b986253589ff98d5151fd
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-09 14:20:22 +0530

    customer's call to acition is added

---
+++ 3next-auth/components/Home/CustomerRe.tsx
@@ -0,0 +1,56 @@
+
+export default function CustomerReview() {
+    return <div>
+         <div className="py-9" >
+          <div className=" flex justify-between mt-28">
+            <div className=" text-5xl font-semibold">How our costomers use Ninox</div>
+           <div className=" flex gap-6">
+           <div className=" border-2 bg-blue-600 rounded-full p-3" >😶‍🌫️</div>
+           <div className=" border-2 bg-blue-600 rounded-full p-3" >🥶</div>
+           </div>
+          </div>
+          <div className=" mt-12">
+            <div className=" flex border rounded-lg drop-shadow-xl ">
+              <div className=" w-[45%] shadow-xl flex ">
+                <div className=" ml-16 my-20">
+                <div className=" text-2xl  font-semibold">Managing bussiness <br /> operations with teams <br />across geographies</div>
+                 <div className=" mt-12 flex items-center">
+                  <div className=" p-3 rounded-full bg-slate-200 text-xl">💀</div>
+                  <div className=" ml-4 text-[12px] font-[500]">Martin Baeuerie <br /> <span className=" text-[10px] font-extralight opacity-80">Office Manager at Cundall SRL</span></div>
+                 </div>
+                </div> 
+              </div>
+              <div className="w-[55%]">Image</div>
+            </div>
+            </div>
+
+          <div className="mt-14 flex justify-between">
+
+            <div className=" max-w-xs  py-6">
+              <div className="m-5 max-w-60">
+                <div className="text-5xl">🧰</div>
+                <div className="mt-8 text-xl font-semibold opacity-70">Buid Custom Apps</div>
+                <div className=" mt-4 text-xs font-medium opacity-65">Drag & drop to quickly arrange forms, charts and reports</div>
+              </div>
+            </div>
+
+            <div className=" max-w-xs  py-6">
+              <div className="m-5 max-w-60">
+                <div className="text-5xl">🛗</div>
+                <div className="mt-8 text-xl font-semibold opacity-70">Buid Custom Apps</div>
+                <div className=" mt-4 text-xs font-medium opacity-65">Drag & drop to quickly arrange forms, charts and reports</div>
+              </div>
+            </div>
+
+            <div className=" max-w-xs  py-6">
+              <div className="m-5 max-w-60">
+                <div className="text-5xl">🔍</div>
+                <div className="mt-8 text-xl font-semibold opacity-70">Buid Custom Apps</div>
+                <div className=" mt-4 text-xs font-medium opacity-65">Drag & drop to quickly arrange forms, charts and reports</div>
+              </div>
+            </div>
+        
+            </div>
+         </div>
+    </div>
+}
