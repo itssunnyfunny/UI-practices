06d4917f927628136d53ffe2d8b71480d369df2c
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-11 11:14:19 +0530

    Fixed Footer bug

--- 3next-auth/app/page.tsx
+++ 3next-auth/app/page.tsx
@@ -4,19 +4,22 @@ import GetStared from "@/components/Home/GetStared";
 import Landing from "@/components/Home/Landing";
 import UserNo from "@/components/Home/UserNo";
 import UserReview from "@/components/Home/UserReview";
+import Footer from '@/components/Home/Footer'
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div className=" min-h-screen grid grid-cols-[200px_1fr_200px]  bg-slate-100 text-black ">
-    <div className="col-start-2 mb-8 ">
+    <div className="col-start-2 ">
 
         <Landing />
         <CustomerReview />
        <GetStared />
       <UserReview />
       <UserNo />
+     
        </div>
+        <div className="col-start-1 col-end-4 bottom-0">< Footer /></div>
    </div>
   );
 }
