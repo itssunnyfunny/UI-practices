0b49b699711a4b21b9adefdaf4b34319525de0a9
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-04-07 19:29:04 +0530

    rethinking about the projects

--- 2next-pages/app/about/page.jsx
+++ 2next-pages/app/about/page.jsx
@@ -1,8 +1,16 @@
+"use client"
+import Button from '@/components/button'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function page() {
+   const router = useRouter()
   return (
-    <div>page</div>
+    <div >page
+      <Button onClick={()=> router.push("/")}></Button>
+      <button onClick={() => router.push("/")}>click here</button>
+      
+    </div>
   )
 }
 
